aca6e775c2608caf83b19ea4060adb60589c3caf
EFE SAMED KIRLI <efesamed@outlook.com.tr>
2023-04-05 17:05:14 +0300

    Add existing project files to Git

---
+++ app.js
@@ -0,0 +1,8 @@
+function imgSlider(anythhing){
+    document.querySelector(".starbucks").src =anythhing
+}
+
+function changeCircleColor(color){
+    const circle = document.querySelector(".circle")
+    circle.style.background = color ;
+}
